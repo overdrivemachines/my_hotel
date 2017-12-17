1a35c52a2508f807d9ad1b9cac84bab247e1593b
Dipen Chauhan <get.dipen@gmail.com>
2017-12-16 22:26:25 -0800

    fixing push

--- p1/db/seeds.rb
+++ p1/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p = Property.new
+p.name = Faker::Company.name
+p.address1 = Faker::Address.street_address
+p.address2 = ""
+p.city = Faker::Address.city
+p.state = Faker::Address.state
+p.zip = Faker::Address.zip
+p.phone = Faker::PhoneNumber.phone_number
+p.fax = Faker::PhoneNumber.phone_number
+p.email = Faker::Internet.email
+p.check_in_at = "15:00"
+p.check_out_at = "11:00"
+p.night_audit_auto = [true, false].sample
+p.night_audit_time = "04:00"
+p.night_audit_emails = Faker::Internet.email
+p.print_reg_cards_auto = [true, false].sample

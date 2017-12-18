9f7dee417fe6218e6262b460b55ee5979e2126d8
Dipen Chauhan <get.dipen@gmail.com>
2017-12-18 11:33:38 -0800

    Default values

--- p1/app/models/property.rb
+++ p1/app/models/property.rb
@@ -24,17 +24,28 @@
 
 # TODO: complete validations
 class Property < ApplicationRecord
+	before_save :default_values
+
 	validates :name, presence: true, length: 2..50
 	validates :address1, presence: true, length: 2..50
 	validates :city, presence: true, length: 2..50
 	validates :state, presence: true, length: 2..50
-	validates :zip, presence: true, length: {minimum: 2..20}
+	validates :zip, presence: true, length: 2..20
 	validates :phone, presence: true, length: {minimum: 2}
 	validates :email, presence: true, length: {minimum: 2}
 	validates :check_in_at, presence: true
 	validates :check_out_at, presence: true
-	validates :night_audit_auto, presence: true
+	# validates :night_audit_auto, presence: true
 	validates :night_audit_time, presence: true
-	validates :night_audit_emails, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-	validates :print_reg_cards_auto, presence: true
+	validates :night_audit_emails, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+	# validates :print_reg_cards_auto, presence: true
+
+	def default_values
+		if (self.night_audit_auto == nil)
+			self.night_audit_auto = false
+		end
+		if (self.print_reg_cards_auto == nil)
+			self.night_audit_auto = false
+		end
+	end
 end

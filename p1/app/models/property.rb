1188a1171e214712f9aeeb5a2bcaecb5f7acd413
Dipen Chauhan <get.dipen@gmail.com>
2017-12-18 10:31:48 -0800

    Added basic validations

--- p1/app/models/property.rb
+++ p1/app/models/property.rb
@@ -1,2 +1,40 @@
+# == Schema Information
+#
+# Table name: properties
+#
+#  id                   :integer          not null, primary key
+#  name                 :string
+#  address1             :string
+#  address2             :string
+#  city                 :string
+#  state                :string
+#  zip                  :string
+#  phone                :string
+#  fax                  :string
+#  email                :string
+#  check_in_at          :time
+#  check_out_at         :time
+#  night_audit_auto     :boolean
+#  night_audit_time     :time
+#  night_audit_emails   :string
+#  print_reg_cards_auto :boolean
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+
+# TODO: complete validations
 class Property < ApplicationRecord
+	validates :name, presence: true, length: 2..50
+	validates :address1, presence: true, length: 2..50
+	validates :city, presence: true, length: 2..50
+	validates :state, presence: true, length: 2..50
+	validates :zip, presence: true, length: {minimum: 2..20}
+	validates :phone, presence: true, length: {minimum: 2}
+	validates :email, presence: true, length: {minimum: 2}
+	validates :check_in_at, presence: true
+	validates :check_out_at, presence: true
+	validates :night_audit_auto, presence: true
+	validates :night_audit_time, presence: true
+	validates :night_audit_emails, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+	validates :print_reg_cards_auto, presence: true
 end
